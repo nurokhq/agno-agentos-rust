fd6c2099d98226e33725c835ed60a2710112b873
jasonaix <zr@nurok.ai>
2025-12-11 16:47:03 +0800

    change check_dirty_files.sh

--- scripts/check_dirty_files.sh
+++ scripts/check_dirty_files.sh
@@ -2,8 +2,7 @@
 
 ./scripts/generate_api_sdk.sh
 
-if [ -n "$(git status --porcelain)" ]; then
-    echo "Error: Generated files are not up to date. Please run ./scripts/generate_api_sdk.sh and commit the changes."
-    git status --porcelain
+if git status --porcelain | grep -q "M"; then
+    echo "Error: There are dirty files"
     exit 1
 fi
